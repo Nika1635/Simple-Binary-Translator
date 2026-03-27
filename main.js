9c6815367beb79258222acf44f0cfa7dc944d6ff
nika robakidze <nika70252@gmail.com>
2026-03-27 17:21:24 +0400

    translate-to-character

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let binaryInputElemnt = document.getElementById("binaryInput")
 let binaryElement = document.getElementById("binaryText")
 let translateBinary = document.getElementById("translateBinary")
 
-let binaryText = '213123 13213123'
+let binaryText = ''
 let binaryChunks = binaryText.split(" ")
 
 binaryInputElemnt.addEventListener("input", (e) => {
@@ -11,13 +11,17 @@ binaryInputElemnt.addEventListener("input", (e) => {
 })
 
 translateBinary.addEventListener('click', async (e) => {
-    let binaryChunksFilter = binaryChunks.filter(item => item.split("").every(char => char === "0" || char === "1"))
-
-    binaryChunks.map(item => {
-        if(item.length === 8){
-            console.log(binaryChunksFilter)
-        } else{
-            return alert("one byte should be 8 character")
-        }
+    let binaryChunksFilter = binaryChunks.filter(item => item.split("").every(char => char === "0" || char === "1") && item.length === 8)
+    let stringDecimal = ""
+    let character = ""
+    binaryChunksFilter.map(item => {
+            let decimal = parseInt(item, 2)
+            stringDecimal += decimal.toString() + " "
+            character += String.fromCharCode(decimal) + " "
+            console.log(item)
     })
+
+    stringDecimal.trim()
+
+    binaryElement.innerText = `binary: ${binaryChunksFilter} decimals: ${stringDecimal} characters: ${character}`
 })
